3758d71c253532d101fb1e26d6a4cab4496675d5
Tim Lum <twhlum@gmail.com>
2017-10-22 13:22:10 -0700

    Create Driver.cpp

---
+++ Driver.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "BCD.h"
+
+using namespace std;
+
+int main() {
+   int a = 125;
+   int b = 975;
+   
+   BCD n1;
+   cout << n1 << endl;
+/*   BCD n2(a);
+
+   cout << "Converted 125 (int) to " << n2 << " BCD" << endl;
+
+   n1 = n2 + b;
+   cout << n2 << " + 975 (int) = " << n1 << endl;
+
+   cin >> n2;
+   cout << "Input " << n2 << endl;
+
+   BCD n3 = 64;
+   cout << "Converted 64 (int) to " << n3 << " BCD" << endl;
+
+   BCD sum = n1 + n3;
+   BCD difference = n1 - n3;
+   BCD product = n1 * n3;
+   BCD quotient = n1 / n3;
+   cout << n1 << " + " << n3 << " = " << sum;
+   cout << n1 << " - " << n3 << " = " << difference;
+   cout << n1 << " * " << n3 << " = " << product;
+   cout << n1 << " / " << n3 << " = " << quotient;
+
+   cout << product << " has " << product.numDigits() << " digits" << endl;
+   */
+
+   return (0);
+}
