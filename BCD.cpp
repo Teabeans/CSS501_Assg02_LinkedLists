5e1c4b32b682f1bed721c5041b499edacdd37386
Tim Lum <twhlum@gmail.com>
2017-10-24 19:48:52 -0700

    Update BCD.cpp

--- BCD.cpp
+++ BCD.cpp
@@ -121,22 +121,22 @@ using namespace std;
 // ---- STRUCTS ----
 
 struct BCDnode {
-      // Fields: Node fields
-      string nodeName = "";
-      int data = NULL;
-      int borrow = 0;
-      int carry = 0;
-      BCDnode* moreSDptr; // Node pointer to the next more significant digit (next)
-      BCDnode* lessSDptr; // Node pointer to the next least significant digit (prev)
+   // Fields: Node fields
+   string nodeName = "";
+   int data = NULL;
+   int borrow = 0;
+   int carry = 0;
+   BCDnode* moreSDptr; // Node pointer to the next more significant digit (next)
+   BCDnode* lessSDptr; // Node pointer to the next least significant digit (prev)
 
 
 
 
-   }; // Closing 'struct Node'
+}; // Closing 'struct Node'
 
-// Note: Fields can be initialized in EITHER the .h or the .cpp
+   // Note: Fields can be initialized in EITHER the .h or the .cpp
 
-// ---- FIELDS ----
+   // ---- FIELDS ----
 BCDnode* headptr;
 bool isPositive = true; // Sign to determine whether this BCD is positive or negative
 int length = 1;
@@ -144,7 +144,7 @@ int length = 1;
 // ---- CONSTRUCTORS ----
 // BCD::BCD() - Constructs a new BCD with a value of "0"
 // "In the BCD namespace, method named BCD receiving (<no arguments>), initializing 'head' to 'nullptr' and executing {<no commands>}
-BCD::BCD( ) { // By default, the node pointer (BCDnode*) named 'head' is initialized to nullptr
+BCD::BCD() { // By default, the node pointer (BCDnode*) named 'head' is initialized to nullptr
    headptr = new BCDnode();
    BCDnode* body = new BCDnode(0, headptr, headptr); // Generates new node with value of 0, headptr set to both 'next' and 'prev'
    headptr->lessSDptr = body;
@@ -153,29 +153,27 @@ BCD::BCD( ) { // By default, the node pointer (BCDnode*) named 'head' is initial
 
 // BCD::BCD(int someInt) - Constructs a new BCD with an equivalent value to the received argument int
 BCD::BCD(int someInt) {
+   // Determine if the passed int is positive or negative
+   if (someInt < 0) {
+      isPositive = false;
+      // Reverse the sign of the received int so BCD formation is performed with all positive nodes
+      cout << "Achtung! This is negative!" << endl;
+      someInt = someInt * -1;
+   }
+
    // Make the head node
    headptr = new BCDnode();
    // Generates new node with value of the LSD of the int passed, headptr set to both 'next' and 'prev'
    BCDnode* body = new BCDnode((someInt % 10), headptr, headptr);
    headptr->lessSDptr = body;
-   headptr->moreSDptr = body;
-   someInt = someInt / 10;
+   headptr->moreSDptr = body; // 4 of 4 pointers are set
+   someInt = someInt / 10; // Divide
 
-   while (someInt > 0) {
+   while (someInt > 0) { // Breaks after Most SigDigit is divided by 10
       insertMSD(someInt % 10); // Remainder of the int when divided by 10 is always the least significant digit
-      someInt = someInt / 10;
+      someInt = someInt / 10; // Drop the Least SigDigit from someInt
       cout << someInt << endl;
-      // 1234 % 10 = 4
-      // '4'
-      // 123 % 10 = 3
-      // '3-4'
-      // 12 % 10 = 2
-      // '2-3-4'
-      // 1 % 10 = 1
-      // '1-2-3-4'
-      // 0 - Loop breaks
    }
-   
 }
 
 // BCD::BCD(const BCD&) - Constructs a new BCD that is a deep copy of the received argument BCD reference
@@ -192,12 +190,12 @@ BCD::~BCD() {
    /*
    // To create a BCDnode instance:
    BCDnode *newNode = new BCDnode;
-   
+
    Node* tempptr;
    for (; headptr; headptr = tempptr)
    {
-      tempptr = headptr -> next;
-      delete headptr;
+   tempptr = headptr -> next;
+   delete headptr;
    } */
 }
 
@@ -237,18 +235,60 @@ ostream& operator<< (ostream& coutStream, BCD& someBCD) { // where 'cinData' is 
 // Return value: A new BCD object representing the summed addition
 // Functions called: BCD::BCD(int) - Converts an int to a BCD object
 BCD operator+(BCD& term1BCD, BCD& term2BCD) { // where 'someInt' is the input variable and 'thisBCD' is the original BCD object
-   // Term1BCD exists
-   // Term2BCD exists
+                                              // Term1BCD exists
+                                              // Term2BCD exists
+
+
    cout << "Wheeee!" << endl;
    BCD sumBCD; // Generate a sumBCD with value set to 0.
-   // < Implement addition logic here >
-   // Step 1, set current for all BCDs to Least SigDigit
-   // currT1 = term1BCD.headptr->moreSDptr;
-   // currT2 = term2BCD.headptr->moreSDptr;
-   // currSum = sumBCD.headptr->moreSDptr;
-
+               // < Implement addition logic here >
+               // Step 1, set current for all BCDs to Least SigDigit
+               // currT1 = term1BCD.headptr->moreSDptr;
+               // currT2 = term2BCD.headptr->moreSDptr;
+               // currSum = sumBCD.headptr->moreSDptr;
+   if (term1BCD.isPositive && term2BCD.isPositive) {
+      sumBCD = term1BCD.add(term2BCD);
+   }
+   else if (term1BCD.isPositive && !term2BCD.isPositive || !term1BCD.isPositive && term2BCD.isPositive) {
+      // Determine which is greater, send that first
+      // Determine final sign, reserve it
+      bool reserveSign;
+      sumBCD = term1BCD.subtract(term2BCD);
+      sumBCD.isPositive = reserveSign;
+   }
+   else {
+      sumBCD = term1BCD.add(term2BCD);
+   }
    return sumBCD;
 }
+
+// = - Custom behavior for the assignment operator when dealing with a BCD object (left) and a BCD object (right)
+// Parameters: oldBCD - The original BCD object being overwritten ; newBCD - The BCD number being assigned.
+// Preconditions: None
+// Postconditions: A new BCD object exists representing the summed addition of the old BCD and received BCD.
+// Return value: A new BCD object representing the summed addition
+// Functions called: BCD::BCD(int) - Converts an int to a BCD object
+/*
+BCD operator=(BCD& oldBCD, BCD& newBCD) { // where 'someInt' is the input variable and 'thisBCD' is the original BCD object
+                                              // Term1BCD exists
+                                              // Term2BCD exists
+   if oldBCD.headptr == newBCD.headptr{
+      // < These are the same thing
+      // break;
+   }
+   cout << "Wheeee!" << endl;
+   BCD sumBCD; // Generate a sumBCD with value set to 0.
+               // < Implement addition logic here >
+               // Step 1, set current for all BCDs to Least SigDigit
+               // currT1 = term1BCD.headptr->moreSDptr;
+               // currT2 = term2BCD.headptr->moreSDptr;
+               // currSum = sumBCD.headptr->moreSDptr;
+   BCD::BCDnode* T1Curr = term1BCD.headptr->lessSDptr;
+   BCD::BCDnode* T2Curr = term1BCD.headptr->lessSDptr;
+   BCD::BCDnode* sumCurr = sumBCD.headptr->lessSDptr;
+   return sumBCD;
+}
+*/
 
 //const BCD& operator=(const BCD&) {
 // Test that "this != RightHandArgument" - If same, you'll end up deleting the thing, then filling it with its deleted self
@@ -264,6 +304,19 @@ BCD operator+(BCD& term1BCD, BCD& term2BCD) { // where 'someInt' is the input va
 
 // TODO: Implement clear() - Deletes all nodes and deallocates memory
 
+// TODO: Implement add(BCD, BCD)
+BCD add(BCD& term2BCD) {
+   cout << "term1, node1: " << BCD::this->
+   cout << "term2, node1: " << term2BCD.headptr->moreSDptr->data << endl;
+   int sum = (this.headptr->moreSDptr->data) + (term2BCD.headptr->moreSDptr->data);
+   cout << "sum of T1N1 and T2N1: " << sum << endl;
+   return term2BCD;
+}
+// TODO: Implement subtract (BCD, BCD)
+BCD subtract(BCD& term1BCD, BCD& term2BCD) {
+
+}
+
 // isLastNode() - Declares whether the node in question is the last node in a linked list (.next or .prev leads to a node with a null value)
 // Parameters: No internal fields
 // Preconditions: None
@@ -275,7 +328,7 @@ const bool isLastNode(BCDnode* someNodeptr) {
    bool isLast = false;
    // Test criteria, can be returned directly (TODO?)
    // If the passed node's lessSDptr or moreSDptr point to the head, then this is a last node.
-   if (someNodeptr -> lessSDptr == headptr || someNodeptr -> moreSDptr == headptr) {
+   if (someNodeptr->lessSDptr == headptr || someNodeptr->moreSDptr == headptr) {
       isLast = true;
    }
    // Return flag status
@@ -292,9 +345,9 @@ const void BCD::insertMSD(int someData) {
    // Make a new node off in space. Its data is the argument passed. Its MSD pointer points to head, its LSD pointer points to the old MSD.
    BCDnode* insertedNodeptr = new BCDnode(someData, headptr, headptr->lessSDptr);
    // Redirects the old MSD's next pointer to the inserted node
-   headptr->lessSDptr->moreSDptr = insertedNodeptr; 
+   headptr->lessSDptr->moreSDptr = insertedNodeptr;
    // Redirect the head's lessSDptr to the current node.
-   headptr->lessSDptr = insertedNodeptr; 
+   headptr->lessSDptr = insertedNodeptr;
    // New node created with int data and 4 of 4 pointers are pointed at the correct place
 }
 
